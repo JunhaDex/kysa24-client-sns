0e46750adcdbd68e67701c36fb04e6f8f350456c
JunhaDex <dk.junha.dev@gmail.com>
2024-06-19 22:46:56 +0900

    add firebase deploy

---
+++ src/providers/firebase.provider.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from '@/../secret/firebase-config.json'
+
+export class FirebaseProvider {
+  private readonly app
+
+  constructor() {
+    this.app = initializeApp(firebaseConfig)
+  }
+}
